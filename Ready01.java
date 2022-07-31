f44aaaab5a17e5b5ef0fb5747657a899ee5a9d65
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-07-31 14:08:05 +0000

    dmm java.compile

--- Ready01.java
+++ Ready01.java
@@ -1,4 +1,4 @@
-public class Ready02 {
+public class Ready01 {
     public static void main(String[] args) {
         System.out.println("Hello WEBCAMP!");
         System.out.println("Hello Java!");
---
+++ Ready02.java
@@ -0,0 +1,12 @@
+public class Ready02 {
+	public static void main(String[] args) {
+		String name = "Kato Yuya";
+		System.out.println(name);
+
+		name = "vic";
+		System.out.println(name);
+
+		final int NUMBER = 7;
+		System.out.println(NUMBER);
+	}
+}
