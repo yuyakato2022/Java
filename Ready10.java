76c1067f351755a0415b708f154dd08d66a0ad35
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-10 13:27:02 +0000

    dmm practice

--- Ready10.java
+++ Ready10.java
@@ -1,41 +1,32 @@
-public class While01 {
-    public static void main(String[] args) {
-        int number = 1;
-        // while文：ループ開始
-        // numberが50未満の場合、ブロックの処理を行なう
-        while (number < 50) {
-            // ブロック：
-            // numberにnumber * 2 を代入して、numberを出力
-            number *= 2;
-            System.out.println("While01 = " + number);
-        }
-    }
-}
+package exception;
 
-public class DoWhile01 {
+public class Exception03 {
     public static void main(String[] args) {
-        int number = 1;
-        // do-while文：ループ開始
-        do {
-            // ブロック：
-            // numberにnumber * 2 を代入して、numberを出力
-            number *= 2;
-            System.out.println("DoWhile01 = " + number);
-        // do-while文：
-        // numberが50未満の場合、ブロックの処理をループ実行する
-        } while (number < 50);
-    }
-}
-
-public class For01 {
-    public static void main(String[] args) {
-        // for文：
-        // 初期化式：numberを1で初期化
-        // 条件式：numberが5未満の場合、ブロックの処理を行う
-        // 変化式：ブロックの処理後に、numberをインクリメント
-        for (int number = 1; number <= 5; number++) {
-            // ブロック：numberを出力
-            System.out.println("For01 = " + number);
+        try {
+            // divisionメソッドを呼び出し
+            Exception03.division(100, 0);
+        // ArithmeticExceptionクラスの例外が発生した場合の例外処理
+        } catch (ArithmeticException e) {
+            // 例外クラスが一致しないので、例外処理が実行されない
+            System.out.println("ArithmeticException例外が発生");
+        // IllegalArgumentExceptionクラスの例外が発生した場合の例外処理
+        } catch (IllegalArgumentException e) {
+            // 例外処理が実行される
+            System.out.println("IllegalArgumentException例外が発生");
+            // キャッチした例外インスタンスをスロー
+            throw e;
         }
+        // finallyブロックでないので処理が実行されない
+        System.out.println("プログラム終了");
+    }
+
+    public static void division(int a, int b) {
+        System.out.println(a + " ÷ " + b + " は？");
+        if (b == 0) {
+            // 割り算で例外が発生するので、意図的にIllegalArgumentException例外をスロー
+            throw new IllegalArgumentException("引数が不正です");
+        }
+        int result = a / b;
+        System.out.println("計算結果" + result);
     }
 }
