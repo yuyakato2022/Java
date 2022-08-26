7e2a4fce4c196db5f3d4088feba27a01f08e5168
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-26 12:47:17 +0000

    1day practice

---
+++ Sample101.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Sample101 {
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+		System.out.print(123);
+		System.out.println(456);
+		System.out.print("ABC");
+		System.out.println("DEF");
+	}
+}
