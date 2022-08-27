592f0120aed2b54c3aa00f73aea4a4ff35afb1e7
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-27 05:18:56 +0000

    3day practice

---
+++ Sample301.java
@@ -0,0 +1,24 @@
+package day3;
+ 
+public class Sample301 {
+ 
+    public static void main(String[] args) {
+        //標準出力
+        int a = 10; //  整数値（いろいろ変えてみましょう）
+        System.out.println("a="+a);
+        //  入力した値が、正の数かどうかを調べる
+        if(a > 0){
+            System.out.println("aは正の数です。");  //  正の数だった場合に実行
+        }
+    }
+    
+        int a = -5; //  整数値（いろいろ変えてみましょう）
+        System.out.println("a="+a);
+        //  入力した値が、正の数かどうかを調べる
+        if(a > 0){
+            System.out.println("aは正の数です。");  //  正の数だった場合に実行
+        }else{
+            System.out.println("aは正の数ではありません。");        //  正の数ではなかった場合
+        }
+ 
+}
